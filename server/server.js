a1e683426d38e1fbdeccdb1cf976af06f790ae73
pratiush776 <pratiush776@gmail.com>
2024-11-05 17:22:42 -0500

    version_1.0_cors_fix_try_2

--- server/server.js
+++ server/server.js
@@ -1,17 +1,34 @@
 const express = require("express");
 const app = express();
 const cors=require("cors");
-const corsOptions ={
-    origin: ["https://homedoc-rfey.onrender.com/"]
+const allowedOrigins ={
+    origin: ["https://homedoc-rfey.onrender.com/","http://localhost:5173/"]
 };
 const main = require("./api")
 
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
+
+// CORS configuration
+const corsOptions = {
+    origin: function (origin, callback) {
+      // Allow requests with no origin (like mobile apps or curl requests)
+      if (!origin) return callback(null, true);
+      if (allowedOrigins.indexOf(origin) !== -1) {
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed by CORS'));
+      }
+    },
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'], // Add other headers if needed
+    credentials: true, // If you need to send cookies or auth headers
+  };
+
 app.use(cors(corsOptions));
 
-
+app.options('*', cors(corsOptions));
 app.post("/api", async (req, res) => {
     console.log("API called");
     console.log(req.body);
