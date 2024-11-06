1c8e3d821d18b36d586ec6d0e4b026003a69dfbf
pratiush776 <pratiush776@gmail.com>
2024-11-05 20:53:07 -0500

    button_fix

--- client/src/components/Information.jsx
+++ client/src/components/Information.jsx
@@ -126,6 +126,7 @@ const Information = ({setResult}) => {
                 </div>
 
                 <button
+                type='submit'
                     className="block mx-auto bg-indigo-600 text-white py-2 px-4 my-8 rounded-md text-2xl
                     hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2
                     sm:w-full sm:max-w-xs sm:py-4 sm:px-6 sm:text-xl sm:rounded-lg"
