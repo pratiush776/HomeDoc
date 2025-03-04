e00f76ebbbf92a95d8a784a23ba6c27561bf417f
pratiush776 <pratiush776@gmail.com>
2025-03-03 20:32:30 -0500

    groq_model_fix

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const app = express();
 const cors=require("cors");
 const main = require("./api")
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 9000;
 
 app.use(express.static('public'));   
 app.use(express.json());
